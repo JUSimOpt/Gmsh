548143ed1e1b82768f3da1e8114feab4ce5de947
mirza <mirza.cenanovic@ju.se>
2017-12-01 10:39:00 +0100

    Development file

--- main.m
+++ main.m
@@ -2,7 +2,7 @@ clear;clc;close all
 
 addpath(fullfile('Gmsh'))
 
-[status,cmdout] = RunGmshScript('beamMeshGenerator.geo','verbose','on')
+[status,cmdout] = RunGmshScript('beamMeshGenerator.geo','verbose','on');
 
-msh = MshRead(fullfile(pwd,'mesh.msh'))
+msh = MshRead(fullfile(pwd,'mesh.msh'),'typesToExtract',[3,5])
 
